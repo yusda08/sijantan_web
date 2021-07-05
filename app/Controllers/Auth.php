2eaf0393e82f63e5dd7ce544479d99bcfa87bf56
yusda08 <yusdahelmani@gmail.com>
2021-07-05 10:55:06 +0800

    asa

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -59,7 +59,7 @@ class Auth extends BaseController
         } catch (\Exception $th) {
             $dataArray = $this->setResponse($th->getMessage());
         }
-        return $this->respond($dataArray);
+        return $this->respond($dataArray, $dataArray['status']);
     }
 
     function register()
